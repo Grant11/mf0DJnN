7bc7844461b8e06e96900a324f4e6e91094b7126
Mr. Unknown <MasterM@contractor.net>
2016-05-23 17:36:16 -0400

    Update JOIN SERVER.bat
    Updated it to fit TTU.

--- win32/JOIN SERVER.bat
+++ win32/JOIN SERVER.bat
@@ -4,8 +4,8 @@ set /P user="Username: " || ^
 set /P pass="Password: " || ^
 set user=username
 set pass=password
-set TTR_PLAYCOOKIE=%ttrUsername%
-set TTR_GAMESERVER=127.0.0.1
+set TTU_PLAYCOOKIE=%ttrUsername%
+set TTU_GAMESERVER=127.0.0.1
 
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
